d5a4c7edf46c882b4d467a48d983eaed531c4777
Maxim Vilchevsky <maxmaxwow@gmail.com>
2017-06-09 13:50:25 +0300

    added project

---
+++ src/main/java/com/maxvi/Main.java
@@ -0,0 +1,11 @@
+package com.maxvi;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("DOWNLOADER");
+        if (args.length != 0) {
+            System.out.println(args[0]);
+        }
+    }
+}
